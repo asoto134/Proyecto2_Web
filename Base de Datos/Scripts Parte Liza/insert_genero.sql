fdcf5b40ee1e8c0f7818e4ad864202e0a64729fa
Liza Chaves <lizchavca@gmail.com>
2015-05-24 22:45:44 -0600

    Revert "Scripts de Insersión"
    This reverts commit 98982de2e5d9b6f477808d70022b5bf2219dfcb6.

--- Base de Datos/Scripts Parte Liza/insert_genero.sql
+++
@@ -1,7 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE add_genero (IN id_familia int, IN genero varchar(50))
-BEGIN
-    insert into genero (Familia_idFamilia, Genero) 
-	
-    values (id_familia, genero);
-END //
--- Base de Datos/Scripts Parte Liza/insert_tipo_pico.sql
+++ Base de Datos/Scripts Parte Liza/insert_tipo_pico.sql
@@ -1,7 +1,8 @@
 DELIMITER //
-CREATE PROCEDURE add_tipo_pico (IN tipo_pico varchar(50))
+CREATE PROCEDURE tipo_pico ()
 BEGIN
-    insert into tipo_de_pico (Tipo_de_Pico) 
+    insert into pajaro (idPajaro, Especie_idEspecie, Nombre_Comun, Nombre_en_Ingles, Nombre_Cientifico,
+						Tipo_de_Pico_idTipo_de_Pico, Cantidad_de_Huevos) 
 	
-    values (tipo_pico);
+    values (id_Pajaro , idEspecie, nombre_comun, nombre_ingles, nombre_cientifico, id_Tipo_Pico, cantidad_huevos);
 END //
