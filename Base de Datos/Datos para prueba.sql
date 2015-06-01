9cc87cf7d91c57960bf8ca310d5818988a981b18
Amanda <sofia.29@hotmail.es>
2015-06-01 11:15:25 -0600

    Inserts de datos para pruebas
    Datos aleatorios para probar la base. Recordar poner otros en el
    proyecto ya listo

---
+++ Base de Datos/Datos para prueba.sql
@@ -0,0 +1,4 @@
+CALL `proyecto2web`.`REGISTRO`(1, 'Sujeto1', 'Sujeto1', 'S1','p' , '3333', 'correosujeto1', 1)
+CALL `proyecto2web`.`REGISTRO`(2, 'Sujeto2', 'Sujeto2', 'S2','p' , '3333', 'correosujeto2', 2)
+CALL `proyecto2web`.`REGISTRO`(3, 'Sujeto3', 'Sujeto3', 'S3','p' , '3333', 'correosujeto3', 1)
+CALL `proyecto2web`.`REGISTRO`(4, 'Sujeto4', 'Sujeto4', 'S4','p' , '3333', 'correosujeto4', 2)
